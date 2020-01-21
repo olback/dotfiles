fa0b7b1a3c28474d4822b6375af61dddbc023733
Edwin Svensson <git@olback.net>
2020-01-21 21:25:26 +0100

    update structure

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-FILES=$(ls ~/.dotfiles | grep -v install.sh | grep -v dotfiles | grep -v LICENSE | grep -v README.md)
+FILES=$(ls ~/.dotfiles/shell)
 
 rm -f ~/.dotfiles/dotfiles
 
 for f in $FILES; do
-    echo "source /home/$USER/.dotfiles/$f" >> ~/.dotfiles/dotfiles
+    echo "source /home/$USER/.dotfiles/shell/$f" >> ~/.dotfiles/dotfiles
 done
 
 echo "Add 'source /home/$USER/.dotfiles/dotfiles' to .zshrc or .bashrc"
