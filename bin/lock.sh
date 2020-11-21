e7dd63402dfae10a1d7f2de0be5aa2ca76868a3f
Edwin Svensson <git@olback.net>
2020-11-21 20:26:38 +0100

    use prcoessed screenshot as lock background

--- bin/lock.sh
+++ bin/lock.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-swaylock
+images=()
+swaylock_args=()
+
+for output in $(swaymsg -t get_outputs | jq -r '.[] .name'); do
+  image=$(mktemp --suffix=.png)
+  images+=($image)
+  swaylock_args+=(-i $output:$image)
+  grim -o $output $image
+done
+
+#printf '%s\n' "${images[@]}" | xargs -P 0 -I{} convert -blur 0x8 {} {}
+for img in ${images[@]}; do
+    corrupter -mag 2 -boffset 25 $img $img
+done
+
+
+swaylock ${swaylock_args[@]} -s center
+rm ${images[@]}
 
