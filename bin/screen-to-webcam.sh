864828a6c8ead250fc370040f39ef481b38b691b
Edwin Svensson <git@olback.net>
2021-05-24 11:51:02 +0200

    load kernel module if not already loaded

--- bin/screen-to-webcam.sh
+++ bin/screen-to-webcam.sh
@@ -6,5 +6,15 @@
 # If not already configured, load required kernel module like this:
 # modprobe v4l2loopback exclusive_caps=1 card_label=WfRecorder 
 
+MODNAME=v4l2loopbackk
+
+modinfo $MODNAME > /dev/null
+
+if [[ $? != 0 ]]; then
+    echo "Loading kernel module $MODNAME"
+    sudo modprobe $MODNAME
+
+fi
+
 wf-recorder --muxer=v4l2 --codec=rawvideo --file=/dev/video2 -x yuv420p
 
