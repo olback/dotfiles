d172fbe3e210dd1cbb487f2600ae2a144406865d
Edwin Svensson <git@olback.net>
2021-12-20 16:16:00 +0100

    add todo comment, remove old sway stuff

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ FILES=$(ls ~/.dotfiles/shell)
 
 rm -f ~/.dotfiles/dotfiles
 
+# TODO: Link configs
+
 for f in $FILES; do
     echo "source /home/$USER/.dotfiles/shell/$f" >> ~/.dotfiles/dotfiles
 done
