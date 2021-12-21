79d335399689e079806664e230d45fc667035ef2
Edwin Svensson <git@olback.net>
2021-12-21 23:21:30 +0100

    120hz

--- bin/sway-layout.sh
+++ bin/sway-layout.sh
@@ -3,7 +3,9 @@
 HOSTNAME=$(cat /etc/hostname)
 
 if [ $HOSTNAME = "EDWINS-PC" ]; then
-    swaymsg 'output HDMI-A-5 pos 0 200' && swaymsg 'output DP-3 pos 1680 0'
+    swaymsg 'output HDMI-A-5 pos 0 200'
+    swaymsg 'output DP-3 pos 1680 0'
+    swaymsg 'output DP-3 mode 2560x1440@119.998Hz'
     #swaymsg 'output HDMI-A-5 pos 0 0' && swaymsg 'output DP-3 pos 1680 0'
 elif [ $HOSTNAME = "T14G2" ]; then
     # TODO
