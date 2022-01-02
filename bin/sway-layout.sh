7dfe4586f17826c8f4d70cb9cbc52f1d4d03ed5a
Edwin Svensson <git@olback.net>
2022-01-02 17:18:24 +0100

    update sway layout script

--- bin/sway-layout.sh
+++ bin/sway-layout.sh
@@ -8,8 +8,8 @@ if [ $HOSTNAME = "EDWINS-PC" ]; then
     swaymsg 'output DP-3 mode 2560x1440@119.998Hz'
     #swaymsg 'output HDMI-A-5 pos 0 0' && swaymsg 'output DP-3 pos 1680 0'
 elif [ $HOSTNAME = "T14G2" ]; then
-    # TODO
-    echo 'todo'
+    swaymsg 'output DP-5 pos 0 130'
+    swaymsg 'output DP-7 pos 1920 0 res 2560x1440@165.080Hz'
+    swaymsg 'output eDP-1 pos 4480 180'
 fi
-
 
